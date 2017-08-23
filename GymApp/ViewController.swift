2f20705c244a5a29dc659d26bc440336521db1f1
Mauricio Chirino <mauricio@3codegeeks.com>
2017-08-23 07:26:34 -0400

    Initial Commit

---
+++ GymApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GymApp
+//
+//  Created by Mauricio Chirino on 23/8/17.
+//  Copyright © 2017 3CodeGeeks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
