6452a619b6af8d337b1af31dffce4b8275ec1a8e
Mauricio Chirino <mauricio@3codegeeks.com>
2017-08-24 10:44:50 -0400

    Added launch screen

---
+++ GymApp/LaunchController.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchController.swift
+//  GymApp
+//
+//  Created by Mauricio Chirino on 24/8/17.
+//  Copyright © 2017 3CodeGeeks. All rights reserved.
+//
+
+import UIKit
+
+class LaunchController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
