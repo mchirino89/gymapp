20ef030236badab5551f3aac144cfc3d924b1d8a
Mauricio Chirino <mauricio@3codegeeks.com>
2017-08-27 22:44:43 -0400

    Removed useless event for textfield in profile view

--- GymApp/ProfileController.swift
+++ GymApp/ProfileController.swift
@@ -43,7 +43,4 @@ class ProfileController: UIViewController {
             self.view.sendSubview(toBack: self.genderVisualEffectView)
         })
     }
-    
-    @IBAction func nameAction() {
-    }
 }
